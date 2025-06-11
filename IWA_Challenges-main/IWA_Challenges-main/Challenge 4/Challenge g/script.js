ae46d7edc6ad6dfc1b6e9abf87a7111cade656b8
Mark <mcschaefer26@gmail.com>
2025-06-11 22:09:00 +0200

    Challenge 4 g - mostly declaring variables in the correct place and correcting statement logic

--- IWA_Challenges-main/IWA_Challenges-main/Challenge 4/Challenge g/script.js
+++ IWA_Challenges-main/IWA_Challenges-main/Challenge 4/Challenge g/script.js
@@ -1,41 +1,49 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders';
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence';
-NONE_SELECTED = '0';
+const FREE_WARNING = 'Free shipping only applies to single customer orders';
+const BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence';
+const NONE_SELECTED = 0;
 // Moved this code from the bottom and declared the variables 
 const customers = '1';
 const location = 'RSA';
 let currency = null;
+let shipping = null;
 
 if (location == 'RSA') {
-	let shipping = 400;
+	shipping = 400;
 	currency = 'R';
-}
-
-if (location == 'NAM') {
+} else if (location == 'NAM') {
 	shipping = 600
+	currency = '$';
 } else {
 	shipping = 800
+	currency = '$'
 }
 
-shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
-pens = 5 * 'NONE_SELECTED' 
 
-shipping = null
-currency = $
 
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
+let shoes = 300 * 1;
+let toys = 100 * 5;
+let shirts = 150 * NONE_SELECTED;
+let batteries = 35 * 2;
+let pens = 5 * NONE_SELECTED; 
+
+let cost = shoes + batteries + pens + shirts + toys;
+//currency = $
+
+if (customers === '1' && (cost >= 1000) && currency == 'R') {
+	shipping = 0;
+	if (location == 'NAM' && currency == '$' && cost >= 60) {
+		shipping = 0;
+			//if (location = RSA) {
+			//	shipping = 0 || calcShipping
+			//}
+	} else {
+		shipping = 800;
 	}
-}
+};
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+if (shipping == 0 && customers !== '1') { console.log(FREE_WARNING) };
+
+location == 'NK' ? console.log(BANNED_WARNIN) : console.log('price', currency, (cost + shipping));
 
 
